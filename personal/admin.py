235442938378e12b68a8c239926f33e050125168
Alberto Bernal <smb34abr@gmail.com>
2026-01-05 09:02:07 -0600

    Se registro el módulo de contacto

--- personal/admin.py
+++ personal/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Contact
 
 # Register your models here.
+@admin.register(Contact)
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'created')
+    search_fields = ('name', 'email')
+    list_filter = ('created',)
+    ordering = ('name',)
+    
