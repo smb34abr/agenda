86e3ff449abe78de27da7645b7b5eac3443bc288
Alberto Bernal <smb34abr@gmail.com>
2026-01-05 09:45:43 -0600

    Se crea la vista se agerag datos a la base

---
+++ personal/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = 'personal'
+
+urlpatterns = [
+    path('', views.contact_list_and_create, name='contact_list'),
+]
--- personal/views.py
+++ personal/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Contact
+from .forms import ContactForm
 
 # Create your views here.
+def contact_list_and_create(request):
+    form = ContactForm()
+    contacts = Contact.objects.all()
+
+    return render(request, 'contact_list.html', {
+    'form': form, 
+    'contacts': contacts
+    })
