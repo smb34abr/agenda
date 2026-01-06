1a9884c3d6e56e89894f3e2206cdf6f7bd122dba
Alberto Bernal <smb34abr@gmail.com>
2026-01-05 18:23:11 -0600

    Se le agrego la funcionalidad de grabado

--- personal/views.py
+++ personal/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Contact
 from .forms import ContactForm
 
 # Create your views here.
 def contact_list_and_create(request):
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('personal:contact_list')
+
     form = ContactForm()
     contacts = Contact.objects.all()
 
