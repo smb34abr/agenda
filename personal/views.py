1dcafdb620724416eca533e075b94402d67a20a0
Alberto Bernal <smb34abr@gmail.com>
2026-01-06 22:57:09 -0600

    Crear la funcionalidad de visualizacion de los registro para su edición

--- personal/views.py
+++ personal/views.py
@@ -32,6 +32,10 @@ def registrarPersonal(request):
     messages.success(request, 'Contacto agregado correctamente')
     return redirect('/')
 
+def editContact(request, id):
+    contact = Contact.objects.get(id=id)
+    return render(request, 'edit_contact.html', {'contact': contact})
+
 def deleteContact(request, id):
     contact = Contact.objects.get(id=id)
     contact.delete()
